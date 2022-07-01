c526129e2e848e2501d3caf815e0d57dfc1a23e9
Pobarul-islam <pobarul3@gmail.com>
2022-07-01 22:49:50 +0600

    local server fetched

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -9,7 +9,17 @@ const Todo = () => {
 
         // send data to the server
 
-        
+        fetch('http://localhost:5000/user', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+            console.log('success', data)
+        })
     }
     return (
         <div>
